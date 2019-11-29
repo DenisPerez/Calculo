929573b7a5ffd52e9d37e2519aec13076086f965
DenisPerez <denisadrian@gmail.com>
2019-11-29 17:51:56 -0500

    ITS A FRACTAL

--- Taller1/ejercicio4.py
+++ Taller1/ejercicio4.py
@@ -4,31 +4,52 @@ from matplotlib import pyplot as plt
 #import numpy as np
 import scipy as s
 import random
+import numpy as np
+
+A1 = np.array([[0.85,0.04],[-0.04,0.85]])
+A2 = np.array([[0.20,-0.26],[0.23,0.22]])
+A3 = np.array([[-0.15,0.28],[0.26,0.24]])
+A4 = np.array([[0,0],[0,0.16]])
+b1 = np.array([[0],[1.6]])
+b = np.array([[0],[0.44]])
+
+x = np.array([[0.5],[0.5]])
 
 
-w = s.linspace(0,1,50)
-n = 1000
+#w = s.linspace(0,1,50)
+n = 300000
 
 xvalues = []
 yvalues = []
 
 def transformacion(n):
+    x = np.array([[0.5],[0.5]])
     for i in range(n):
         r = random.uniform(0,1)
         if(r < 0.85):
-            xvalues.append(0.445) # A1x + b1
-            yvalues.append(2.005)
+            temp = np.add(np.dot(A1,x),b1)
+            xvalues.append(temp[0]) # A1x + b1
+            yvalues.append(temp[1])
+            x = temp
+
         elif(r < 0.92):
-            xvalues.append(-0.03) # A2x + b2
-            yvalues.append(0.665)
+            temp = np.add(np.dot(A2,x),b)
+            xvalues.append(temp[0]) # A1x + b1
+            yvalues.append(temp[1])
+            x = temp
+
         elif(r < 0.99):
-            xvalues.append(0.065) # A3x + b3
-            yvalues.append(0.665)
+            temp = np.add(np.dot(A3,x),b)
+            xvalues.append(temp[0]) # A1x + b1
+            yvalues.append(temp[1])
+            x = temp
+
         elif(r <= 1):
-            xvalues.append(0)     # A4x
-            yvalues.append(0.08)
+            temp = np.dot(A4,x)
+            xvalues.append(temp[0]) # A1x + b1
+            yvalues.append(temp[1])
+            x = temp
 
 transformacion(n)
-plt.scatter(xvalues,yvalues)
-plt.scatter([0.5],[0.5])
+plt.scatter(xvalues,yvalues,color = "green")
 plt.show()
