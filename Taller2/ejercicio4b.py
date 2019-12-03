a3681c830d22302494fc6629d8579efe34636405
DenisPerez <denisadrian@gmail.com>
2019-12-02 22:33:59 -0500

    Lab finalizado

--- Taller2/ejercicio4b.py
+++ Taller2/ejercicio4b.py
@@ -1,18 +1,13 @@
-# -*- coding: utf-8 -*-
 import numpy as np
 
 f = lambda x : (x-np.sin(x))/(x**3)
 fvalues = []
-
 def evaluatef(n):
-    for x in range(1,n+1):
-        if x == 1:
+    x = 1
+    for i in range(1,n+1):
+        if i == 1:
             fvalues.append(f(1))
         else:
             x = (1/10)*x
             fvalues.append(f(x))
-        
-        
-        
-    
-evaluatef(15)
+evaluatef(60) 
