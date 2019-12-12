dc1d7dee01cbafe63e61a44017900612829e6803
DenisPerez <denisadrian@gmail.com>
2019-12-12 17:42:55 -0500

    Ejericicio1c.py

--- Taller2/ejercicio1c.py
+++ Taller2/ejercicio1c.py
@@ -3,9 +3,12 @@ import numpy as np
 
 
 acum = np.log(6/5)
-print(acum)
 n = 0
 
-for i in range(1,20):
+for i in range(5,25,5):
     acum = (1/i)-(5*acum)
-    print(acum)
+    #print(i,":", acum)
+    
+for i in range(30,70,10):
+    acum = (1/i)-(5*acum)
+    #print(i, acum)
