1ebdf4d65091ec83e6261d57a534768f5432fa70
DenisPerez <denisadrian@gmail.com>
2019-12-12 13:25:07 -0500

    Lab2

--- Taller2/ejercicio1c.py
+++ Taller2/ejercicio1c.py
@@ -6,6 +6,11 @@ acum = np.log(6/5)
 print(acum)
 n = 0
 
-for i in range(1,20):
+
+for i in range(5,25,5):
+    acum = (1/i)-(5*acum)
+    print(acum)
+    
+for i in range(30,70,10):
     acum = (1/i)-(5*acum)
     print(acum)
--- Taller2/ejercicio2.py
+++ Taller2/ejercicio2.py
@@ -6,11 +6,11 @@ x = np.array([0.2]*n)
 norma1 = 0
 print(x)
 
-for k in range(0,n):
+for k in range(0, n):
     norma1 = norma1 + x[k]
     print(np.abs(x[k]))
 print(norma1)
-if norma1==(0.2*n):
+if norma1 == (0.2*n):
     print(0.2*n)
     print("Calcule la norma 1 de x")
 else:
