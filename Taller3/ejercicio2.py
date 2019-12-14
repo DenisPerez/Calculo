1cf5ae38aa96247cdb82763a885fcde80630abe5
Heyzel <49635289+Heyzel@users.noreply.github.com>
2019-12-14 18:33:14 -0400

    Tridiagonal

---
+++ Taller3/ejercicio2.py
@@ -0,0 +1,29 @@
+import numpy as np
+from pprint import pprint
+
+def y(a,b,c):
+    n = len(a)
+    arr = []*n
+    arr[0] = a[0]
+    for i in range(1,n-1):
+        arr[i] = a[i] - c[i-1]*b[i-1]/arr[i-1]
+    return arr
+
+def tridiag(beta,b,c):
+    n = len(beta)
+    L = np.identity(n)
+    U = np.identity(n)
+    for i in range (n-1):
+        L[i+1][i] = c[i]/beta[i]
+        U[i][i] = beta[i]
+        U[i][i+1] = b[i]
+    U[n-1][n-1] = beta[n-1]
+    return L, U
+    
+a = [1,2,3,4]
+b = [2,2,2,2]
+c = [3,3,3,3]
+
+L, U = tridiag(y(a,b,c), b, c)
+pprint(L)
+pprint(U)
