e40f0d4743e9cff5722525ccba694903a1891f97
DenisPerez <denisadrian@gmail.com>
2019-12-14 17:02:32 -0500

    factorizacion back LU con separacion de triangular inferior(L) y triangular superior unitaria (U)

--- Taller3/ejercicio1.py
+++ Taller3/ejercicio1.py
@@ -2,11 +2,13 @@
 
 from pprint import pprint
 import numpy as np
-n = 4
-#Number of rows and columns off the matrix A
+from upper_lower import upper, lower
 
+#Number of rows and columns off the matrix A
+n = 10
 #FUNCION LAMBDA PARA CREAR MATRICES DE LA FORMA PARTICULAR DE LA PREGUNTA
 y = lambda i, j: -1 if i < j  else (1 if i == j or i == (n-1)  else 0)
+#z = lambda i,j: 1 if i == j else 0
 #CREACION DE LA MATRIZ A
 A = [[y(i,j) for i in range(n)]for j in range(n)]
 def factLUBack(A):
@@ -17,6 +19,17 @@ def factLUBack(A):
             for j in range(k-1, -1, -1):
                 A[i][j] = A[i][j]-A[i][k]*A[k][j]
     return(np.array(A))
-    
-U = factLUBack(A)
+
+matrix = factLUBack(A)
+#pprint(matrix)
+L = lower(matrix,n,n)
+U = upper(matrix,n,n)
+
+print("L:")
+pprint(L)
+print("\n")
+print("U:")
 pprint(U)
+print("\n")
+print("A = UL:")
+pprint(np.dot(U,L))
