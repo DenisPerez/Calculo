52fe329c02846760cdedd85165539ce62ab0b3a7
DenisPerez <denisadrian@gmail.com>
2019-12-14 16:03:47 -0500

    Genera matrices como las del enunciado con una fucnion lambda y lo imprime bonito

--- Taller3/ejercicio1.py
+++ Taller3/ejercicio1.py
@@ -1,26 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from pprint import pprint
+import numpy as np
+n = 4
+#Number of rows and columns off the matrix A
 
-A = [[1,2,3],[4,5,7],[9,8,7]]
-
+#FUNCION LAMBDA PARA CREAR MATRICES DE LA FORMA PARTICULAR DE LA PREGUNTA
+y = lambda i, j: -1 if i < j  else (1 if i == j or i == (n-1)  else 0)
+#CREACION DE LA MATRIZ A
+A = [[y(i,j) for i in range(n)]for j in range(n)]
 def factLUBack(A):
-    
     n = len(A)
-    
     for k in range(n-1, -1, -1):
         for i in range(k-1, -1, -1):
-            #print(A[i][k])
             A[i][k] = A[i][k]/A[k][k]
-            #print(A[k][k])
-            
             for j in range(k-1, -1, -1):
-                #print(A[i][j])
                 A[i][j] = A[i][j]-A[i][k]*A[k][j]
-                #print(A[i][j])
-            #print("\n")
-    
-    return(A)
+    return(np.array(A))
     
 U = factLUBack(A)
 pprint(U)
