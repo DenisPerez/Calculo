e6993cfb09a5ab9ad114c02b64a0375080b024cf
DenisPerez <denisadrian@gmail.com>
2019-12-14 15:04:51 -0500

    Cholesky completo. Casos de prueba y funcion de validacion

--- Taller3/ejercicio4.py
+++ Taller3/ejercicio4.py
@@ -3,29 +3,34 @@
 import scipy.linalg as sla
 from math import sqrt
 from pprint import pprint
+import numpy as np
 
 
 def cholesky(A):
-    n = len(A)
+    n = len(A) #tamano de la matriz a procesar
     
-    L = [[0.0]* n for i in range(n)]
+    L = [[0.0]* n for i in range(n)]#iniciaizo L como una matriz de todo 0
     
     
     for i in range(n):
-        for k in range(i+1):
-            
-            tmp_sum = sum(L[i][j] * L[k][j] for j in range(k))
-            
+        for k in range(i+1):  
+            back_list = [L[i][j] * L[k][j] for j in range(k)]
+            tmp_sum = sum(back_list)
             if (i==k):
-                #LaTeX: l_{kk} = \sqrt{ a_{kk} - \sum^{k-1}_{j=1} l^2_{kj}}
+                #Me encuentro en una diagonal y por lo tanto solo tengo que elevar al cuadrado
                 L[i][k] = sqrt(A[i][i] - tmp_sum)
             else:
-                #LaTeX: l_{ik} = \frac{1}{l_{kk}} \left( a_{ik} - \sum^{k-1}_{j=1} l_{ij} l_{kj} \right)
+                #Tengo que restar todos los elementos anteriores al valor de A y tengo que sacar raiz
                 L[i][k] = (1.0 / L[k][k] * (A[i][k] - tmp_sum))
-    return L
+    return (np.array(L)).transpose()#Retorno la matriz traspuesta para comprobar los resultados
+                                    #Con la matriz resultante de sla.cholesky
 
-
-A = [[6,3,4,8], [3,6,5,1], [4,5,10,7], [8,1,7,25]]
+# Matrices de la practica para los ejemplos
+#A = np.array([[4,1,-1], [-1,4.25,2.75], [1,2.75,4]])
+#A = np.array([[25,25,25],[25,50,50],[25,50,75]])
+A = np.array([[25,-25,-25],[-25,50,0],[-25,0,75]])
+#A = np.array([[25,25,25],[25,106,106],[25,106,6667]])
+#A = np.array([[4,-6,14],[-6,34,-34],[14,-34,339]])
 
 L = cholesky(A)
 
@@ -33,7 +38,8 @@ L = cholesky(A)
 print ("A:")
 pprint(A)
 print("L:")
-pprint(L)
 
+pprint(L)
+print("\n")
 L = sla.cholesky(A)
 pprint(L)
