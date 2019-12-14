efc7f1f127bac793a5427332bcac88b54baf27b7
Heyzel <49635289+Heyzel@users.noreply.github.com>
2019-12-14 19:07:54 -0400

    Trigiadonal arreglada

--- Taller3/ejercicio2.py
+++ Taller3/ejercicio2.py
@@ -3,9 +3,9 @@ from pprint import pprint
 
 def y(a,b,c):
     n = len(a)
-    arr = []*n
+    arr = [None]*n
     arr[0] = a[0]
-    for i in range(1,n-1):
+    for i in range(1,n):
         arr[i] = a[i] - c[i-1]*b[i-1]/arr[i-1]
     return arr
 
@@ -24,6 +24,12 @@ a = [1,2,3,4]
 b = [2,2,2,2]
 c = [3,3,3,3]
 
+#m = 5
+#a = [1 for i in range(m)]
+#b = [2 for i in range(m)]
+#c = [2 for i in range(m)]
+
 L, U = tridiag(y(a,b,c), b, c)
 pprint(L)
+print()
 pprint(U)
