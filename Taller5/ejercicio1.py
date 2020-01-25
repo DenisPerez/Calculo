0cb50e27812726ad7f8213bd3da42d621aba075f
Heyzel <49635289+Heyzel@users.noreply.github.com>
2020-01-24 22:09:31 -0400

    Tabla de numero de condicion y normas de forbenius

---
+++ Taller5/ejercicio1.py
@@ -0,0 +1,55 @@
+import numpy as np
+import scipy.linalg as sla
+from pprint import pprint
+from B_ejercicio1 import B
+
+A = np.random.rand(5,5)
+Q, R = np.linalg.qr(A)
+Be = B(A, np.identity(5), Q, R)
+K_fro = np.linalg.cond(A, p = 'fro')
+Norm_froB = np.linalg.cond( (np.identity(5) - np.dot(A,Be)), p = 'fro')
+A_inv = sla.inv(A)
+Norm_fro = np.linalg.cond( (np.identity(5) - np.dot(A,A_inv)), p = 'fro')
+
+results = np.zeros((4,3))
+results[0][0] = K_fro
+results[0][1] = Norm_froB
+results[0][2] = Norm_fro
+
+A = sla.hilbert(5)
+K_fro = np.linalg.cond(A, p = 'fro')
+Q, R = np.linalg.qr(A)
+Be = B(A, np.identity(5), Q, R)
+Norm_froB = np.linalg.cond( (np.identity(5) - np.dot(A, Be)), p = 'fro')
+A_inv = sla.invhilbert(5)
+Norm_fro = np.linalg.cond( (np.identity(5) - np.dot(A,A_inv)), p = 'fro')
+
+results[1][0] = K_fro
+results[1][1] = Norm_froB
+results[1][2] = Norm_fro
+
+A = np.random.rand(20,20)
+K_fro = np.linalg.cond(A, p = 'fro')
+Q, R = np.linalg.qr(A)
+Be = B(A, np.identity(20), Q, R)
+Norm_froB = np.linalg.cond( (np.identity(20) - np.dot(A, Be)), p = 'fro')
+A_inv = sla.inv(A)
+Norm_fro = np.linalg.cond( (np.identity(20) - np.dot(A, A_inv)), p = 'fro')
+
+results[2][0] = K_fro
+results[2][1] = Norm_froB
+results[2][2] = Norm_fro
+
+A = sla.hilbert(20)
+K_fro = np.linalg.cond(A, p = 'fro')
+A_inv = sla.invhilbert(20)
+Q, R = np.linalg.qr(A)
+Be = B(A, np.identity(20), Q, R)
+Norm_froB = np.linalg.cond( (np.identity(20) - np.dot(A, Be)), p = 'fro')
+Norm_fro = np.linalg.cond( (np.identity(20) - np.dot(A, A_inv)), p = 'fro')
+
+results[3][0] = K_fro
+results[3][1] = Norm_froB
+results[3][2] = Norm_fro
+
+pprint(results)
