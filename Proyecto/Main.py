7890bc478c506b8049633034463e2637b9a6e2a1
DenisPerez <denisadrian@gmail.com>
2020-03-11 22:02:51 -0400

    Derivada de C

--- Proyecto/Main.py
+++ Proyecto/Main.py
@@ -8,6 +8,7 @@ from scipy import optimize
 C = lambda h1,h2,p1,p2,s,x : (p1*h1/np.math.sqrt((h1**2 + x**2)**3)) + (p2*h2/np.math.sqrt(h2**2+(s-x)**2)**3)
 f1 = lambda h1,h2,p1,p2,s,x : (p1*h1/np.math.sqrt((h1**2 + x**2)**3))
 f2 = lambda h1,h2,p1,p2,s,x : (p2*h2/np.math.sqrt(h2**2+(s-x)**2)**3)
+derC = lambda h1,h2,p1,p2,s,x : -((3*p1*h1*x*(x**2+h1**2)**2)/((h1**2+x**2)**3)**(3/2)) + ( (3*h2*p2*((-x+s)**2 + h2**2)**2 * (-x+s)) / ((h2**2 + (s-x)**2)**3)**(3/2))
 
 #Constant values
 h1 = 5
@@ -17,17 +18,20 @@ p2 = 2000
 s = 10
 
 #Distantce in X
-x = np.arange(-30,11,1) 
+x = np.arange(0,11,1) 
 
 #Y values
 y1 = [f1(h1,h2,p1,p2,s,i) for i in x]
 y2 = [f2(h1,h2,p1,p2,s,i) for i in x]
 c = [C(h1,h2,p1,p2,s,i) for i in x]
+dc = [derC(h1,h2,p1,p2,s,i) for i in x]
+
 
 #plot functions
-plt.plot(x,y1,'-')
-plt.plot(x,y2,'-')
-plt.plot(x,c,'-')
+plt.plot(x,y1,'o-')
+plt.plot(x,y2,'--')
+plt.plot(x,c,'--o')
+plt.plot(x,dc,'*-')
 
 
 
